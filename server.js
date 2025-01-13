8b92ced4a56febbd738cc43c2dde184c962d8f27
heilotiitsmaa <heilo.tiitsmaa@voco.ee>
2025-01-13 22:04:22 +0200

    ...-query strings

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 8000; //8000 is fallback
 
 const app = express();
 
+/* //static web server with node.js:
 //setup static folder
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
 //app object-> Get method: respond to a get request to '/' (root V endpoint), 
 // sec argument of f: f takes in a req & res obj
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+}); */
+
+let posts = [
+    {id: 1, title: 'Post 1'},
+    {id: 2, title: 'Post 2'},
+    {id: 3, title: 'Post 3'},
+];
+
+//get all posts
+app.get('/api/posts', (req, res) => {
+    console.log(req.query);
+    res.json(posts);
 });
 
-app.listen(8000, () => console.log('Server töötab pordis 8000'));
+//get single post
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt (req.params.id);
+    res.json(posts.filter((post) => post.id === id));
+});
+
+app.listen(port, () => console.log(`Server töötab pordis ${port}`));
